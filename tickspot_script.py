0f679e7f38a18457a6d5568b1566c41f543c6433
Adrian <adrian.hintermaier@tunstallnordic.com>
2019-10-21 09:26:15 +0200

    added support for daterange

--- tickspot_script.py
+++ tickspot_script.py
@@ -1,4 +1,4 @@
-from pprint import pprint
+from datetime import timedelta
 import requests
 import click
 
@@ -8,36 +8,34 @@ API_URL = 'https://www.tickspot.com/79559/api/v2/'
 @click.command()
 @click.option('--token', 'api_token', required=True, help='Your api token')
 @click.option('--email', required=True, help='Your email')
+@click.option('--from_date', type=click.DateTime(['%Y-%m-%d']), required=True, help='Starting day')
+@click.option('--to_date', type=click.DateTime(['%Y-%m-%d']), required=True, help='Last day (including)')
 @click.option('--user_agent', default='MyCoolApp', help='Optional name for User-Agent')
-def main_method(api_token, email, user_agent):
+def main_method(api_token, email, from_date, to_date, user_agent):
     headers = {
         'Authorization': f'Token token={api_token}',
         'User-Agent': f'{user_agent} ({email})',
     }
 
-    payload = {'start_date': "'2019-10-01'", 'end_date': "'2019-10-14'"}
-
-    r = requests.get(API_URL + 'entries.json', headers=headers, params=payload)
-    print(r.text)
-    # pprint(r.json())
-
-    create_entry("'2019-10-14'", 12453101, headers)
+    create_entries(12453101, headers, from_date, to_date)
 
 
-def create_entry(date, task_id, headers, hours=8, notes=""):
+def create_entries(task_id, headers, from_date, to_date, hours=8, notes=""):
     url = API_URL + "entries.json"
-    data = {
-        "date": date,
-        "hours": hours,
-        "notes": notes,
-        "task_id": task_id,
-        # "user_id": 343724
-    }
 
-    r = requests.post(url, json=data, headers=headers)
-    print(r.text)
-    # print(r.headers)
-    pprint(r.json())
+    for single_date in daterange(from_date, to_date):
+        data = {
+            "date": single_date.strftime('%Y-%m-%d'),
+            "hours": hours,
+            "notes": notes,
+            "task_id": task_id,
+        }
+        r = requests.post(url, json=data, headers=headers)
+
+
+def daterange(from_date, to_date):
+    for n in range(int((to_date - from_date).days + 1)):
+        yield from_date + timedelta(n)
 
 
 if __name__ == '__main__':
