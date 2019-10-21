db6091b925bc94efc8d933f24795eb4f6511f7bc
Adrian <adrian.hintermaier@tunstallnordic.com>
2019-10-21 10:38:54 +0200

    split out into subcommand

--- tickspot_script.py
+++ tickspot_script.py
@@ -5,22 +5,28 @@ import click
 API_URL = 'https://www.tickspot.com/79559/api/v2/'
 
 
-@click.command()
+@click.group()
 @click.option('--token', 'api_token', required=True, help='Your api token')
 @click.option('--email', required=True, help='Your email')
-@click.option('--from_date', type=click.DateTime(['%Y-%m-%d']), required=True, help='Starting day')
-@click.option('--to_date', type=click.DateTime(['%Y-%m-%d']), required=True, help='Last day (including)')
 @click.option('--user_agent', default='MyCoolApp', help='Optional name for User-Agent')
-def main_method(api_token, email, from_date, to_date, user_agent):
+@click.pass_context
+def cli(ctx, api_token, email, user_agent):
     headers = {
         'Authorization': f'Token token={api_token}',
         'User-Agent': f'{user_agent} ({email})',
     }
+    ctx.ensure_object(dict)
+    ctx.obj['HEADERS'] = headers
 
-    create_entries(12453101, headers, from_date, to_date)
 
-
-def create_entries(task_id, headers, from_date, to_date, hours=8, notes=""):
+@cli.command()
+@click.option('--task_id', type=click.INT, required=False, help='Id for the task')
+@click.option('--from_date', type=click.DateTime(['%Y-%m-%d']), required=False, help='Starting day')
+@click.option('--to_date', type=click.DateTime(['%Y-%m-%d']), required=False, help='Last day (including)')
+@click.option('--hours', default=8, help='Amount of hours to fill in')
+@click.option('--notes', default="", help='Optional note to fill in')
+@click.pass_context
+def create_entries(ctx, task_id, from_date, to_date, hours, notes):
     url = API_URL + "entries.json"
 
     for single_date in daterange(from_date, to_date):
@@ -30,7 +36,10 @@ def create_entries(task_id, headers, from_date, to_date, hours=8, notes=""):
             "notes": notes,
             "task_id": task_id,
         }
-        r = requests.post(url, json=data, headers=headers)
+        # r = requests.post(url, json=data, headers=ctx.obj['HEADERS'])
+        r = click.echo(url)
+        r = click.echo(data)
+        r = click.echo(ctx.obj['HEADERS'])
 
 
 def daterange(from_date, to_date):
@@ -39,4 +48,4 @@ def daterange(from_date, to_date):
 
 
 if __name__ == '__main__':
-    main_method()
+    cli()
